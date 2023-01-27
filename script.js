ca965cade32f5c4d051f3593ac2ecc5be2a5b3a1
Ryan <98854650+ryalee@users.noreply.github.com>
2023-01-27 10:47:53 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+
+const mario = document.querySelector('.mario' );
+const pipe = document.querySelector('.pipe')
+const jump = () => {
+
+    mario.classList.add('jump'); 
+    
+    setTimeout(() => {
+        mario.classList.remove('jump');
+    }, 500)
+
+}
+
+const loop = setInterval(() => {
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+
+    if(pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+        pipe.style.animation = 'none';
+        pipe.style.left = `${pipePosition}px`;
+
+        mario.style.animation = 'none';
+        mario.style.bottom = `${marioPosition}px`;
+
+        mario.src = '/img/mario-gameover.png';
+        mario.style.width = '75px';
+        mario.style.marginLeft = '50px';
+
+        clearInterval(loop);
+    }
+}, 10)
+
+const reset = document.getElementById('reset-btn');
+
+reset.onclick = function() {
+    var resetGame = document.getElementsByClassName('game-screen');
+
+    
+}
+
+document.addEventListener('keydown', jump);
